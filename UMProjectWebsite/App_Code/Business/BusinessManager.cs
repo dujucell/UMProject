b34936578806b45e6715ffa0acf984c1c49aad8d
sadallo <sadallo@hotmail.com>
2015-05-28 14:14:52 -0400

    Create project

---
+++ UMProjectWebsite/App_Code/Business/BusinessManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UMProjectWebsite.Service.Interface;
+using UMProjectWebsite.Service;
+
+namespace UMProjectWebsite.Business
+{
+    public abstract class BusinessManager
+    {
+        protected IService getService(String name)
+        {
+            return (Factory.getInstance()).getService(name);
+        }
+    }
+}
