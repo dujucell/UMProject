7cd0157888d9fcfa2cd833fe6b1266d4eac5fc82
sadallo <sadallo@hotmail.com>
2015-06-03 11:46:46 -0400

    Custom directories

---
+++ UMWebsite/App_Code/Constants/CustomDirectories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CustomDirectories
+/// </summary>
+public static class CustomDirectories
+{
+    public static string MATLAB_WORKSPACE = "MATLab";
+}
