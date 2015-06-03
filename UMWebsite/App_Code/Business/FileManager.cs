76ac98647ab6c811f43d7de2dca9a037335b61db
gustavohoc <goliveiradacruz@unomaha.edu>
2015-06-03 12:27:15 -0400

    FileService

---
+++ UMWebsite/App_Code/Business/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UMProjectWebsite.Service.Interface;
+
+/// <summary>
+/// Summary description for FactoryManager
+/// </summary>
+
+namespace UMProjectWebsite.Business
+{
+    public class FileManager : BusinessManager
+    {
+        public void MapDirectoryPath()
+        {
+            IFileSystemSvc svcFileSystem = (IFileSystemSvc)this.getService(typeof(IFileSystemSvc).Name);
+        }
+    }
+}
---
+++ UMWebsite/App_Code/Service/Interface/IFileSystemSvc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Summary description for IFileSystemSVC
+/// </summary>
+namespace UMProjectWebsite.Service.Interface
+{
+    
+    public interface IFileSystemSvc : IService
+    {
+        public string MapDirectoryPath(string name); 
+        
+        
+        
+    }
+}
