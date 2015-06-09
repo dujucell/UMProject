6cf6a870b56e061a0e61d6c6046a1d29d8f99fc2
sadallo <sadallo@hotmail.com>
2015-06-09 11:56:00 -0400

    WebService implementation

---
+++ UMRecruiteeWebsite/App_Code/IService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using UMRecruiteeWebsite.DomainDTO;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+    #region Category
+    [OperationContract]
+    List<CategoryDto> selectAllCategory();
+
+    [OperationContract]
+    CategoryDto selectCategoryById(CategoryDto obj);
+
+    [OperationContract]
+    Boolean insertCategory(CategoryDto obj);
+
+    [OperationContract]
+    Boolean updateCategory(CategoryDto obj);
+
+    [OperationContract]
+    Boolean deleteCategory(CategoryDto obj);
+
+    #endregion
+
+    #region Ranking
+    [OperationContract]
+    List<RankingDto> selectAllRanking();
+
+    [OperationContract]
+    RankingDto selectRankingById(RankingDto obj);
+
+    [OperationContract]
+    Boolean insertRanking(RankingDto obj);
+
+    [OperationContract]
+    Boolean updateRanking(RankingDto obj);
+
+    [OperationContract]
+    Boolean deleteRanking(RankingDto obj);
+
+    #endregion
+
+    #region Recruitee
+
+    [OperationContract]
+    List<RecruiteeDto> selectAllRecruitee();
+
+    [OperationContract]
+    RecruiteeDto selectRecruiteeById(RecruiteeDto obj);
+
+    [OperationContract]
+    Boolean insertRecruitee(RecruiteeDto obj);
+
+    [OperationContract]
+    Boolean updateRecruitee(RecruiteeDto obj);
+
+    [OperationContract]
+    Boolean deleteRecruitee(RecruiteeDto obj);
+
+    #endregion
+
+    #region Skill
+    [OperationContract]
+    List<SkillDto> selectAllSkill();
+
+    [OperationContract]
+    SkillDto selectSkillById(SkillDto obj);
+
+    [OperationContract]
+    Boolean insertSkill(SkillDto obj);
+
+    [OperationContract]
+    Boolean updateSkill(SkillDto obj);
+
+    [OperationContract]
+    Boolean deleteSkill(SkillDto obj);
+
+    #endregion
+
+}
---
+++ UMRecruiteeWebsite/App_Code/ServiceMobile.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+using UMRecruiteeWebsite.Business;
+using UMRecruiteeWebsite.DomainDTO;
+using UMRecruiteeWebsite.Models;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceMobile" in code, svc and config file together.
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+public class ServiceMobile : IServiceMobile
+{
+    #region Category
+
+    public List<CategoryDto> selectAllCategory()
+    {
+        CategoryManager mgr = new CategoryManager();
+        List<Category> catList = mgr.selectAllCategory();
+        List<CategoryDto> dtoList = new List<CategoryDto>();
+
+        foreach (Category cat in catList)
+        {
+            dtoList.Add(CategoryDto.createCategoryDTO(cat));
+        }
+
+        return dtoList;
+    }
+
+    public CategoryDto selectCategoryById(String CategoryId)
+    {
+        CategoryManager mgr = new CategoryManager();
+        Category obj = new Category();
+        obj.CategoryId = CategoryId;
+        obj = mgr.selectCategoryById(obj);
+        if (obj != null)
+        {
+            return CategoryDto.createCategoryDTO(obj);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Boolean insertCategory(String CategoryId, String CategoryName, String CategoryDescription)
+    {
+        Category obj = Category.createCategory(CategoryId, CategoryName, CategoryDescription);
+        CategoryManager mgr = new CategoryManager();
+        return mgr.insertCategory(obj);
+    }
+
+    public Boolean updateCategory(String CategoryId, String CategoryName, String CategoryDescription)
+    {
+        Category obj = Category.createCategory(CategoryId, CategoryName, CategoryDescription);
+        CategoryManager mgr = new CategoryManager();
+        return mgr.updateCategory(obj);
+    }
+
+    public Boolean deleteCategory(String CategoryId, String CategoryName, String CategoryDescription)
+    {
+        Category obj = Category.createCategory(CategoryId, CategoryName, CategoryDescription);
+        CategoryManager mgr = new CategoryManager();
+        return mgr.deleteCategory(obj);
+    }
+
+    #endregion
+
+    #region Ranking
+
+    public List<RankingDto> selectAllRanking()
+    {
+        RankingManager mgr = new RankingManager();
+        List<Ranking> ranList = mgr.selectAllRanking();
+        List<RankingDto> dtoList = new List<RankingDto>();
+
+        foreach (Ranking ran in ranList)
+        {
+            dtoList.Add(RankingDto.createRankingDTO(ran));
+        }
+
+        return dtoList;
+    }
+
+    public RankingDto selectRankingById(String RankingId)
+    {
+        RankingManager mgr = new RankingManager();
+        Ranking obj = new Ranking();
+        obj.RankingId = RankingId;
+        obj = mgr.selectRankingById(obj);
+        if (obj != null)
+        {
+            return RankingDto.createRankingDTO(obj);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Boolean insertRanking(String RankingId, String RankingName)
+    {
+        Ranking obj = Ranking.createRanking(RankingId, RankingName);
+        RankingManager mgr = new RankingManager();
+        return mgr.insertRanking(obj);
+    }
+
+    public Boolean updateRanking(String RankingId, String RankingName)
+    {
+        Ranking obj = Ranking.createRanking(RankingId, RankingName);
+        RankingManager mgr = new RankingManager();
+        return mgr.updateRanking(obj);
+    }
+
+    public Boolean deleteRanking(String RankingId, String RankingName)
+    {
+        Ranking obj = Ranking.createRanking(RankingId, RankingName);
+        RankingManager mgr = new RankingManager();
+        return mgr.deleteRanking(obj);
+    }
+
+    #endregion
+
+    #region Recruitee
+
+    public List<RecruiteeDto> selectAllRecruitee()
+    {
+        RecruiteeManager mgr = new RecruiteeManager();
+        List<Recruitee> recList = mgr.selectAllRecruitee();
+        List<RecruiteeDto> dtoList = new List<RecruiteeDto>();
+
+        foreach (Recruitee rec in recList)
+        {
+            dtoList.Add(RecruiteeDto.createRecruiteeDTO(rec));
+        }
+
+        return dtoList;
+    }
+
+    public RecruiteeDto selectRecruiteeById(System.Guid RecruiteeId)
+    {
+        RecruiteeManager mgr = new RecruiteeManager();
+        Recruitee obj = new Recruitee();
+        obj.RecruiteeId = RecruiteeId;
+        obj = mgr.selectRecruiteeById(obj);
+
+        if (obj != null)
+        {
+            return RecruiteeDto.createRecruiteeDTO(obj);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Boolean insertRecruitee(System.Guid RecruiteeId, String RankingId)
+    {
+        Recruitee obj = Recruitee.createRecruitee(RecruiteeId, RankingId);
+        RecruiteeManager mgr = new RecruiteeManager();
+        return mgr.insertRecruitee(obj);
+    }
+
+    public Boolean updateRecruitee(System.Guid RecruiteeId, String RankingId)
+    {
+        Recruitee obj = Recruitee.createRecruitee(RecruiteeId, RankingId);
+        RecruiteeManager mgr = new RecruiteeManager();
+        return mgr.updateRecruitee(obj);
+    }
+
+    public Boolean deleteRecruitee(System.Guid RecruiteeId, String RankingId)
+    {
+        Recruitee obj = Recruitee.createRecruitee(RecruiteeId, RankingId);
+        RecruiteeManager mgr = new RecruiteeManager();
+        return mgr.deleteRecruitee(obj);
+    }
+
+    #endregion
+
+    #region Skill
+
+    public List<SkillDto> selectAllSkill()
+    {
+        SkillManager mgr = new SkillManager();
+        List<Skill> skillList = mgr.selectAllSkill();
+        List<SkillDto> dtoList = new List<SkillDto>();
+
+        foreach (Skill skill in skillList)
+        {
+            dtoList.Add(SkillDto.createSkillDTO(skill));
+        }
+
+        return dtoList;
+    }
+
+    public SkillDto selectSkillById(String SkillId)
+    {
+        SkillManager mgr = new SkillManager();
+        Skill obj = new Skill();
+        obj.SkillId = SkillId;
+        obj = mgr.selectSkillById(obj);
+
+        if (obj != null)
+        {
+            return SkillDto.createSkillDTO(obj);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Boolean insertSkill(String SkillId, String SkillName, String SkillDescription)
+    {
+        Skill obj = Skill.createSkill(SkillId, SkillName, SkillDescription);
+        SkillManager mgr = new SkillManager();
+        return mgr.insertSkill(obj);
+    }
+
+    public Boolean updateSkill(String SkillId, String SkillName, String SkillDescription)
+    {
+        Skill obj = Skill.createSkill(SkillId, SkillName, SkillDescription);
+        SkillManager mgr = new SkillManager();
+        return mgr.updateSkill(obj);
+    }
+
+    public Boolean deleteSkill(String SkillId, String SkillName, String SkillDescription)
+    {
+        Skill obj = Skill.createSkill(SkillId, SkillName, SkillDescription);
+        SkillManager mgr = new SkillManager();
+        return mgr.deleteSkill(obj);
+    }
+
+    #endregion
+   
+}
