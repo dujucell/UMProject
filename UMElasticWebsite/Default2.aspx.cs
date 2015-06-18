84106dfc8b73571fb85c1c58142797e9ca9113c7
sadallo <sadallo@hotmail.com>
2015-06-18 15:05:03 -0400

    Elastic WS update

---
+++ UMElasticWebsite/Default2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UMElasticWebsite.Business;
+using UMElasticWebsite.Models;
+using RecruiteeService;
+using JobService;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //RankingManager mgr = new RankingManager();
+        //List<RankingDto> list = mgr.selectAllRanking();
+
+        //RankingDto obj = new RankingDto();
+        //obj.RankingId = "RAN01";
+        //obj = mgr.selectRankingById(obj);
+
+        SkillRecruiteeManager mgr = new SkillRecruiteeManager();
+        List<RecruiteeService.SkillDto> list = mgr.selectAllSkill();
+
+        //RankingDto obj = new RankingDto();
+        //obj.RankingId = "RAN01";
+        //obj = mgr.selectRankingById(obj);
+
+
+    }
+}
--- UMJobWebsite/Default.aspx.cs
+++ UMJobWebsite/Default.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using MLApp;
 using UMJobWebsite.Business;
 using UMJobWebsite.Models;
+using UMJobWebsite.DomainDTO;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -23,11 +24,16 @@ public partial class _Default : System.Web.UI.Page
         //object[] res = result as object[];
 
         //Label1.Text = res[0] + " " + res[1];
-        JobManager mgr = new JobManager();
-        bool something = mgr.insertJob(Job.createJob(Guid.NewGuid(), "Job name", "COMP9",
-        new Guid("3936A544-59C3-401D-814B-D29C775A81AE"), "Job description", 12, "Job level", 0));
-
+        //JobManager mgr = new JobManager();
+        //bool something = mgr.insertJob(Job.createJob(Guid.NewGuid(), "Job name", "COMP9",
+        //new Guid("3936A544-59C3-401D-814B-D29C775A81AE"), "Job description", 12, "Job level", 0));
         //List<Job> list = mgr.selectAllJob();
+
+        //ServiceMobile svc = new ServiceMobile();
+        //List<SkillDto> list = svc.selectAllSkill();
+
+        SkillManager mgr = new SkillManager();
+        List<Skill> list2 = mgr.selectAllSkill();
         
 
 
